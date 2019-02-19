60175add0136739dcb6afc46e2628472de8871f8
Lucas Silvério <lsilveriomarques@gmail.com>
2019-02-19 16:10:28 -0300

    segundo dia de cangaceiro javascript

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,28 @@
-import { handleStatus, log } from './utils/promise-helpers.js';
+import { log } from './utils/promise-helpers.js';
 import  './utils/array-helpers.js';
+import { NotasService as service} from './nota/service.js'
+
+/*const sumItens = notas => notas.$flatMap(nota => nota.itens)
+.filter( item => item.codigo == '2143')
+.reduce( (total, item) => total + item.valor, 0);*/
+
+/*const sumItens = code => notas => notas.$flatMap(nota => nota.itens)
+.filter( item => item.codigo == code)
+.reduce( (total, item) => total + item.valor, 0);*/
 
 document.querySelector('#myButton')
+.onclick = () => service.sumItens('2143')
+.then(log)
+.catch(log);
+
+/*document.querySelector('#myButton')
+.onclick = () => fetch('http://localhost:3000/notas')
+.then(handleStatus)
+.then(sumItens('2143'))
+.then(log)
+.catch(log);*/
+
+/*document.querySelector('#myButton')
 .onclick = () => fetch('http://localhost:3000/notas')
 .then(handleStatus)
 .then( notas => notas.$flatMap( notas => log(notas.itens)))
@@ -13,6 +34,10 @@ document.querySelector('#myButton')
 .catch(log);
 
 const idades = [26,18,42];
-
 const totals  = idades.reduce( (total, idade) => total += idade, 0);
-console.log(totals);
+*/
+
+const funcaExterna = nome => {
+    const hoje = new Date();
+    return () => console.log(nome, hoje);
+};
---
+++ public/app/nota/service.js
@@ -0,0 +1,18 @@
+import { handleStatus, log } from '../utils/promise-helpers.js';
+
+const API = 'http://localhost:3000/notas';
+
+export const NotasService = {
+
+    listAll() {
+        return fetch(API).then(handleStatus).catch(log);
+    },
+
+    sumItens(code) {
+        return this.listAll().then(
+            notas => notas.$flatMap(nota => nota.itens)
+            .filter( item => item.codigo == code)
+            .reduce( (total, item) => total + item.valor, 0)
+        )
+    }
+}
